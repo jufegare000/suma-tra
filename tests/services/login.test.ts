cd2912f1ccb967b6edc77b524035df7aa8d91af8
“Juan <juanfgallo@experimentality.com>
2021-05-12 18:57:53 -0500

    tests integrated

---
+++ tests/services/login.test.ts
@@ -0,0 +1,13 @@
+import {CreateUserDto} from '../../components/users/model/dto/create-user.dto';
+
+describe('calculate', function() {
+    it('add', function() {
+      const createUserDto: CreateUserDto = {
+          email: '',
+          id: '',
+          pass: '',
+          role:''
+      }
+      console.log(createUserDto);
+    }); 
+  });
